79d978911507496677e3908175aff5e444dd0796
Miro Weißenberg <miro.weissenberg@gmail.com>
2023-07-02 22:31:24 +0200

    initial data scheme

--- flyway/sql/V1___initial_schema.sql
+++ flyway/sql/V1___initial_schema.sql
@@ -1,1 +1,24 @@
-/* Create your schema here */
+CREATE TABLE courses (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+);
+
+CREATE TABLE questions (
+    id SERIAL PRIMARY KEY,
+    course_id INTEGER REFERENCES courses(id),
+    title TEXT NOT NULL,
+    question_content TEXT NOT NULL,
+    posted TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE answers (
+    id SERIAL PRIMARY KEY,
+    question_id INTEGER REFERENCES questions(id),
+    title TEXT NOT NULL,
+    answer_content TEXT NOT NULL,
+    posted TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+/* TODO Create upvote relations */
+
+/* TODO Create meaningful indexes */
