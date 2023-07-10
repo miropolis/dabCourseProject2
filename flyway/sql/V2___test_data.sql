3da1bc183ef5d0d4b8745b6024240801ac08c423
Miro Weißenberg <miro.weissenberg@gmail.com>
2023-07-10 10:01:58 +0200

    limit answers and questions to 20 latest

--- flyway/sql/V2___test_data.sql
+++ flyway/sql/V2___test_data.sql
@@ -6,6 +6,25 @@ INSERT INTO questions (course_id, title, question_content) VALUES (1, 'My first 
 INSERT INTO questions (course_id, title, question_content) VALUES (1, 'My second question', 'This is the content of the question from database');
 INSERT INTO questions (course_id, title, question_content) VALUES (1, 'My third question', 'This is the content of the question from database');
 INSERT INTO questions (course_id, title, question_content) VALUES (1, 'My fourth question', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q5', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q6', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q7', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q8', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q9', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q10', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q11', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q12', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q13', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q14', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q15', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q16', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q17', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q18', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q19', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q20', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q21', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q22', 'This is the content of the question from database');
+INSERT INTO questions (course_id, title, question_content) VALUES (1, 'Q23', 'This is the content of the question from database');
 
 INSERT INTO answers (question_id, title, answer_content) VALUES (1, 'My first answer', 'This is the content of the answer from database');
 INSERT INTO answers (question_id, title, answer_content) VALUES (2, 'My second answer', 'This is the content of the answer from database');
