a2d095918f8c1258beac9f70608714a199416d85
Miro Weißenberg <miro.weissenberg@gmail.com>
2023-07-12 05:16:36 +0200

    reformat tests

--- e2e-playwright/tests/test-page-title.spec.js
+++ e2e-playwright/tests/test-page-title.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
 
 test("Server responds with a page with the title 'Questions & Answers'", async ({ page }) => {
-    await page.goto("/assignment-1/");
-    expect(await page.title()).toBe("Questions & Answers");
-  });
+  await page.goto("/assignment-1/");
+  expect(await page.title()).toBe("Questions & Answers");
+});
