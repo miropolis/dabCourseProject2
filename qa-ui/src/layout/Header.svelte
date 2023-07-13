51b327c7a708fe0990c0a9fad121a99d275ac9e7
Miro Weißenberg <miro.weissenberg@gmail.com>
2023-07-13 09:50:15 +0200

    Removed usuer uuid from header

--- qa-ui/src/layout/Header.svelte
+++ qa-ui/src/layout/Header.svelte
@@ -1,8 +1,3 @@
-<script>
-  import { userUuid } from "../stores/stores.js";
-</script>
-
 <nav class="p-4 mb-4 shadow">
   <span class="text-4xl font-bold"><a href="/">Online Question & Answer Platform</a></span>
-  <span class="text-lg text-gray-700 font-serif float-right">Hello {$userUuid}!</span>
 </nav>
