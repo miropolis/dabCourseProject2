b6c511417193402608bdacf8f38f6bdbc5e32152
Miro Weißenberg <miro.weissenberg@gmail.com>
2023-07-13 11:17:36 +0200

    Ui update

--- qa-ui/src/components/Answer.svelte
+++ qa-ui/src/components/Answer.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="ml-28 mb-4 pb-20 p-6 bg-white border border-gray-200 rounded-lg shadow">
-    <span class="float-right text-sm mr-2">Posted on: {answerDate}</span>
+    <span class="float-right text-sm mr-2">Last updated at: {answerDate}</span>
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{answerTitle}</h5>
     <p class="font-normal text-gray-700">{answerContent}</p>
     <Upvote isQuestion={false} q_a_id={answerID}/>
--- qa-ui/src/components/Question.svelte
+++ qa-ui/src/components/Question.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="mb-4 pb-20 p-6 bg-white border border-gray-200 rounded-lg shadow">
-    <span class="float-right text-sm">Posted on: {questionDate}</span>
+    <span class="float-right text-sm">Last updated at: {questionDate}</span>
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{questionTitle}</h5>
     <p class="font-normal text-gray-700">{questionContent}</p>
     <span class="bg-blue-600"><a href={"/questions/" + questionID}>Show answers!</a></span>
